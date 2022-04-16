489c81a2117f469b252c492127bd86d26b38bf73
robert-virtual <robertocastill63@gmail.com>
2022-04-16 15:53:35 -0600

    nav controller

---
+++ app/src/main/java/com/example/myapplication/BottomSheetFragment.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_bottom_sheet.view.*
+
+
+class BottomSheetFragment : Fragment() {
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+        view.textView2.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_bottomSheetFragment2_to_homeFragment2) }
+        return view
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+
+class HomeFragment : Fragment() {
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        view.textView1.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeFragment2_to_bottomSheetFragment2) }
+        return view
+    }
+
+
+}
