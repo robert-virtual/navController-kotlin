2792ebef21a323a972008c2dd282fe94ef173924
robert-virtual <robertocastill63@gmail.com>
2022-04-16 16:44:51 -0600

    bottom sheet

--- app/src/main/java/com/example/myapplication/BottomSheetFragment.kt
+++ app/src/main/java/com/example/myapplication/BottomSheetFragment.kt
@@ -6,10 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_bottom_sheet.view.*
 
 
-class BottomSheetFragment : Fragment() {
+class BottomSheetFragment : BottomSheetDialogFragment() {
 
 
 
@@ -20,7 +21,7 @@ class BottomSheetFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
-        view.textView2.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_bottomSheetFragment2_to_homeFragment2) }
+        //view.textView2.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_bottomSheetFragment2_to_homeFragment2) }
         return view
     }
 
--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
@@ -20,7 +21,12 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-        view.textView1.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeFragment2_to_bottomSheetFragment2) }
+        //view.textView1.setOnClickListener {
+        // Navigation.findNavController(view).navigate(R.id.action_homeFragment2_to_bottomSheetFragment2)
+        // }
+        view.btn_openSheet.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment2_to_bottomSheetFragment2)
+        }
         return view
     }
 
